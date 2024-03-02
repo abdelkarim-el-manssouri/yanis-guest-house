223c57c60658ce3c116abcc65efdeda4c319ba37
abdelkarim-el-manssouri <elmanssouriabdelkarim0@gmail.com>
2024-03-02 18:06:17 +0100

    hide navbar on scroll

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,19 +1,55 @@
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import { Outlet } from "react-router-dom";
 import "../App.css";
 import { ShiftingDropDown } from "./Navbar";
+import Footer from "./Footer";
+import { useState } from "react";
 
 const DefaultLayout = () => {
+  const [hidden, setHidden] = useState(false);
+  const { scrollY } = useScroll();
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    if (latest > previous && latest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
   return (
     <>
-      <div className="grid-content">
+      <div className="grid-content snap-y">
         {/* To call color from tailwind.config, use -{nameOfKey} ex: text-primary */}
-        <header className="w-full h-screen bg-secondary"></header>
-        <div className="h-screen bg-neutral-950">
+        <header className="w-full h-screen snap-center bg-secondary"></header>
+        <motion.div
+          variants={{
+            visible: { y: 0 },
+            hidden: { y: "-100%" },
+          }}
+          animate={hidden ? "hidden" : "visible"}
+          transition={{ duration: 0.35, ease: "easeInOut" }}
+          className="snap-center bg-neutral-950 sticky top-0"
+        >
           <nav>
             <ShiftingDropDown />
           </nav>
-        </div>
+        </motion.div>
         <Outlet />
+        <footer className="h-screen snap-center">
+          <Footer />
+        </footer>
+        <footer className="h-screen snap-center">
+          <Footer />
+        </footer>
+        <footer className="h-screen snap-center">
+          <Footer />
+        </footer>
+        <footer className="h-screen snap-center">
+          <Footer />
+        </footer>
+        <footer className="h-screen snap-center">
+          <Footer />
+        </footer>
       </div>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: "url('/src/assets/cozy-green-sofa_edited.jpg')",
+        }}
+        className="h-4/5 bg-cover bg-center bg-no-repeat"
+      ></div>
+    </>
+  );
+};
+
+export default Footer;
