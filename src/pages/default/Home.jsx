0764d2ef8152f60a48fe6d0083797a1359d9e627
abdelkarim-el-manssouri <elmanssouriabdelkarim0@gmail.com>
2024-03-13 07:35:02 +0000

    next section done

--- src/pages/default/Home.jsx
+++ src/pages/default/Home.jsx
@@ -17,10 +17,21 @@ const fadeInAnimationVariants = {
   },
 };
 
-const fadeInXAxisAnimationVariants = {
+const fadeInLeftXAxisAnimationVariants = {
   initial: {
     opacity: 0,
     x: -100,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
+const fadeInRightXAxisAnimationVariants = {
+  initial: {
+    opacity: 0,
+    x: 100,
   },
   animate: {
     opacity: 1,
@@ -77,40 +88,45 @@ const Home = () => {
         transition={{ type: "spring", stiffness: 100 }}
         className="h-3/4 grid grid-cols-3 my-16 w-11/12 mx-auto"
       >
-        <div className="h-full w-full flex justify-center items-end relative">
+        <div className="h-full w-full flex lg:justify-center lg:items-end relative">
           {/* <WaterDropGrid /> */}
-          <div className="z-10 absolute top-full md:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/4 md:-translate-y-1/2 w-[90%] md:p-5 mx-auto">
+          <div className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[-25px] md:-translate-y-1/2 w-[90%] md:p-5 mx-auto">
             <motion.h3
-              variants={fadeInXAxisAnimationVariants}
+              variants={fadeInLeftXAxisAnimationVariants}
               initial="initial"
               whileInView="animate"
               transition={{ type: "tween", duration: 1 }}
-              className="text-center mx-auto py-5 md:p-1.5 lg:p-3 my-6 md:my-3 lg:my-5 text-sm md:text-xl lg:text-3xl font-semibold w-[45ch] md:w-[45ch] bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-right-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-right-3 before:top-0"
+              className="text-center mx-auto p-1.5 lg:p-3 mb-5 md:my-3 lg:my-5 text-sm md:text-xl lg:text-3xl font-semibold w-[45ch] md:w-[45ch] bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-right-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-right-3 before:top-0"
             >
               To return to yanis guest house time and time again
             </motion.h3>
-            <motion.p
-              variants={fadeInXAxisAnimationVariants}
+            <motion.div
+              variants={fadeInLeftXAxisAnimationVariants}
               initial="initial"
               whileInView="animate"
               transition={{ type: "tween", duration: 1.5 }}
-              className="p-3 -mt-3 md:mt-0 lg:p-6 mx-auto w-[50ch] md:w-[60ch] lg:w-[75ch] text-xs md:text-sm lg:text-base bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-right-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-right-3 before:top-0"
+              className="p-3 -mt-3 md:mt-0 lg:p-6 mx-auto w-[52ch] md:w-[60ch] lg:w-[75ch] text-xs lg:text-sm tracking-wide bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-right-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-right-3 before:top-0"
             >
-              Our excellence is the result of tireless endeavour, the unfailing
-              commitment of our employees, constant self-questioning, and
-              innovations that are daring yet respectful of the past. This is
-              how yanis guest house remains a place of legend that moves with
-              the times while always remaining in tune with the here and now.
-              Our greatest reward is the loyalty of our customers, who love
-              coming back here because they are made to feel so at home.
-            </motion.p>
+              <p className="mb-2 md:mb-3 lg:mb-4">
+                Our excellence is the result of tireless endeavour, the
+                unfailing commitment of our employees, constant
+                self-questioning, and innovations that are daring yet respectful
+                of the past. This is how yanis guest house remains a place of
+                legend that moves with the times while always remaining in tune
+                with the here and now.
+              </p>
+              <p>
+                Our greatest reward is the loyalty of our customers, who love
+                coming back here because they are made to feel so at home.
+              </p>
+            </motion.div>
           </div>
           <motion.div
             variants={fadeInAnimationVariants}
             initial="initial"
             whileInView="animate"
-            transition={{ type: "tween", duration: 1.8 }}
-            className="absolute lg:relative -bottom-60 md:-bottom-6 lg:bottom-0"
+            transition={{ type: "tween", duration: 1.6 }}
+            className="absolute lg:relative left-9 md:left-20 lg:left-0 -bottom-60 md:-bottom-6 lg:bottom-0"
           >
             <AnimatedButton text="book now">
               <Link to="/book" />
@@ -124,6 +140,66 @@ const Home = () => {
             className="h-full"
             loading="lazy"
           />
+        </div>
+      </motion.section>
+
+      <motion.section
+        variants={fadeInAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        transition={{ type: "spring", stiffness: 100 }}
+        className="h-3/4 grid grid-cols-3 my-16 w-11/12 mx-auto overflow-hidden"
+      >
+        <div className="col-span-3 md:col-span-2">
+          <img
+            src="/src/assets/cozy-green-sofa_edited.jpg"
+            alt="image"
+            className="h-full"
+            loading="lazy"
+          />
+        </div>
+        <div className="h-full w-full flex justify-center items-end relative">
+          <div className="z-10 absolute top-1/2 -right-2 md:right-3/4 md:-translate-x-60 translate-y-[-102%] md:-translate-y-1/2 w-[90%] md:p-5 mx-auto">
+            <motion.h3
+              variants={fadeInRightXAxisAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              transition={{ type: "tween", duration: 1 }}
+              className="mx-auto p-1.5 lg:p-3 my-6 md:my-3 lg:my-5 md:ml-8 lg:ml-0 text-sm md:text-xl lg:text-3xl font-semibold w-[45ch] md:w-[45ch] bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-left-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-left-3 before:top-0"
+            >
+              What makes yanis guest house unique in their eyes?
+            </motion.h3>
+            <motion.div
+              variants={fadeInRightXAxisAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              transition={{ type: "tween", duration: 1.5 }}
+              className="ml-6 md:ml-24 p-3 -mt-3 md:mt-0 lg:p-6 mx-auto w-[50ch] md:w-[60ch] lg:w-[75ch] text-xs lg:text-sm bg-beige relative after:absolute after:w-0.5 after:h-full after:bg-beige after:-left-1.5 after:top-0 before:absolute before:w-1 before:h-full before:bg-beige before:-left-3 before:top-0"
+            >
+              <p className="mb-2 md:mb-3 lg:mb-4">
+                A sense of hospitality that translates into a thousand and one
+                details. From your first visit, we remember your preferences:
+                your ideal room temperature, your favourite coffee, the drinks
+                and dishes you like...
+              </p>
+              <p>
+                So, we know not only what will please you during your next stay,
+                but also what will surprise you and make you want to come back
+                for an experience that is always fresh and new.
+              </p>
+            </motion.div>
+          </div>
+          {/* <motion.div
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            transition={{ type: "tween", duration: 1.6 }}
+            className="absolute lg:relative -bottom-60 md:-bottom-6 lg:bottom-0"
+          >
+            <AnimatedButton text="book now">
+              <Link to="/book" />
+            </AnimatedButton>
+          </motion.div> */}
         </div>
       </motion.section>
     </>
