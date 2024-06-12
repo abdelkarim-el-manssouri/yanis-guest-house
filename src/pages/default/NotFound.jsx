ebe95fb64be328b1a22ef850f0d7bfe266c1d399
abdelkarim-el-manssouri <elmanssouriabdelkarim0@gmail.com>
2024-06-12 14:03:05 +0100

    not found page translated

--- src/pages/default/NotFound.jsx
+++ src/pages/default/NotFound.jsx
@@ -9,6 +9,7 @@ import {
   animate,
 } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const COLORS_TOP = [
   "#073135",
@@ -34,6 +35,7 @@ const AuroraHero = () => {
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
   const border = useMotionTemplate`1px solid ${color}`;
   const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+  const { t } = useTranslation("footer");
 
   return (
     <motion.section
@@ -42,12 +44,12 @@ const AuroraHero = () => {
       }}
       className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
     >
-      <div className="relative z-10 flex flex-col items-center">
+      <div className="relative z-10 flex flex-col items-center !font-Italiana">
         <h1 className="max-w-3xl capitalize bg-gradient-to-br from-background to-cozyGreen bg-clip-text text-transparent text-center text-[8em] lg:-mt-20 font-medium leading-tight sm:text-5xl sm:leading-tight md:text-[14em] md:leading-tight">
           <span className="font-Arapey">404</span>
           <br />
-          <p className="text-4xl md:text-5xl md:-mt-4 mb-20 !font-Montserrat">
-            page not found
+          <p className="text-4xl md:text-5xl md:-mt-4 mb-20 font-black">
+            {t("notFound")}
           </p>
         </h1>
         <Link to="/">
@@ -62,9 +64,9 @@ const AuroraHero = () => {
             whileTap={{
               scale: 0.985,
             }}
-            className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+            className="group relative flex w-fit items-center gap-1.5 !font-PoiretOne font-bold rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
           >
-            Back home
+            {t("back")}
             <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12 mt-0.5" />
           </motion.button>
         </Link>
