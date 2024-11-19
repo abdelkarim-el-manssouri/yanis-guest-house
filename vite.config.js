79c581076c084f44c01e6608ea5f7abdc3f7aa89
abdelkarim el manssouri <elmanssouriabdelkarim0@gmail.com>
2024-11-19 18:27:45 +0100

    test4

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // plugins: [react(), vitePluginRequire.default()],
   plugins: [react()],
-  build: { outDir: "dist" },
+  build: { outDir: "dist", dynamicImportVars: true },
   server: {
     mimeTypes: {
       "image/jpeg": "image/jpeg",
