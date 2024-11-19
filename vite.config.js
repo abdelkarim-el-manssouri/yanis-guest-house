df2e9de8e4b807908ae8bfc7dd90e11e37df938d
abdelkarim el manssouri <elmanssouriabdelkarim0@gmail.com>
2024-11-19 17:18:27 +0100

    test 2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginRequire from "vite-plugin-require";
+// import vitePluginRequire from "vite-plugin-require";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
-  // plugins: [react()],
+  // plugins: [react(), vitePluginRequire.default()],
+  plugins: [react()],
   build: { outDir: "dist" },
+  server: {
+    mimeTypes: {
+      "image/jpeg": "image/jpeg",
+      "image/png": "image/png",
+      "image/jpg": "image/jpg",
+      "image/webp": "image/webp",
+      "text/javascript": "application/javascript",
+    },
+  },
 });
