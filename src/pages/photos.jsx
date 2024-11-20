4ef3ed01b8c9abcac3cd85c671196ada06d96e98
laananihamza <loenwi03@gmail.com>
2024-11-20 12:44:42 +0100

    fix problem of images (solution 1)

--- src/pages/photos.jsx
+++ src/pages/photos.jsx
@@ -16,14 +16,17 @@ const Photos = () => {
 
   useEffect(() => {
     PhotosImages?.map((el) =>
-      el?.data?.map((src, i) =>
-        import(dir + el.folder + src).then((res) =>
-          setUrlArray((prev) => [...prev, res.default])
-        )
+      el?.data?.map((src, i) => {
+        // console.log('src : ' + i , src);
+        // console.log('new URL(`${dir}/gallery/${src}` : ' + i , new URL(`${dir}gallery/${src}`, import.meta.url).href);
+        
+        setUrlArray((prev) => [...prev, new URL(`${dir}gallery/${src}`, import.meta.url).href])
+      }
+        
       )
     );
 
-    setUrlArray([...new Set(url_array)]);
+    // setUrlArray([...new Set(url_array)]);
 
     window.scrollTo({
       top: 0,
