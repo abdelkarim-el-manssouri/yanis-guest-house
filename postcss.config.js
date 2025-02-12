899f0e96d11f2675e5a8784478cb23a267d97816
abdelkarim el manssouri <elmanssouriabdelkarim0@gmail.com>
2025-02-12 14:27:42 +0100

    emprove performance

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,6 @@ export default {
   plugins: {
     "postcss-import": {},
     "tailwindcss/nesting": {},
-    "@fullhuman/postcss-purgecss": {
-      content: ["./src/**/*.jsx", "./src/**/*.js", "./public/index.html"],
-    },
     tailwindcss: {},
     autoprefixer: {},
   },
